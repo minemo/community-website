735e180b14b4b61e244fea7f5a318ea462285f6b
Minemo <wittki56@gmail.com>
2022-01-17 16:47:01 +0100

    Added all the Important stuff

---
+++ pages/i/notfound.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+export default function NotFound() {
+    return (
+        <>
+          <h1>Hier gibt es noch nichts...</h1>
+          <h2>
+            <Link href="/">
+              <a>Back to home</a>
+            </Link>
+          </h2>
+        </>
+      )
+}
